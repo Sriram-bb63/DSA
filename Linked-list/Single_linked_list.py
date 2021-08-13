697530418e455195eba524a973e358727c37dd49
Sriram <invadersriram2002@gmail.com>
2021-08-13 12:39:03 +0530

    Add delete function-Single linked list

--- Linked-list/Single_linked_list.py
+++ Linked-list/Single_linked_list.py
@@ -10,6 +10,12 @@ class linked_list:
     def __init__(self, head = None):
         self.head = head
 
+    def print_linked_list(self):
+        node_iterator = self.head
+        while node_iterator != None:
+            print(node_iterator.value)
+            node_iterator = node_iterator.nextnode
+
     def insert(self, value):
         new_node = node(value)
         if self.head == None:
@@ -20,11 +26,13 @@ class linked_list:
                 node_iterator = node_iterator.nextnode
             node_iterator.nextnode = new_node
 
-    def print_linked_list(self):
+    def delete_node(self, index):
+        i = 0
         node_iterator = self.head
-        while node_iterator != None:
-            print(node_iterator.value)
+        while node_iterator.nextnode != None and i != index - 1:
             node_iterator = node_iterator.nextnode
+            i += 1
+        node_iterator.nextnode = node_iterator.nextnode.nextnode
 
 demo_linked_list = linked_list()
 for i in range(10):
