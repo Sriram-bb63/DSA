4eb6835f01e1bdb917590d8b4ef6196bc34e72ef
Sriram-bb63 <invadersriram2002@gmail.com>
2021-08-16 20:33:23 +0530

    Add stacks

---
+++ Stacks/stack.py
@@ -0,0 +1,33 @@
+class stack:
+	"""
+	Stack implementation using lists
+	"""
+	def __init__(self):
+		self.lst = []
+		self.top = None
+
+	def insert(self, value):
+		self.lst.append(value)
+		self.top = self.lst[-1]
+
+	def pop(self):
+		self.lst.pop()
+		self.top = self.lst[-1]
+
+	def is_empty(self):
+		if len(self.lst) >= 1:
+			return False
+		else:
+			return True
+
+	def peek(self):
+		return self.top
+
+	def print_stack(self):
+		for i in self.lst[-1::-1]:
+			print(i)
+
+x = stack()
+for i in range(10):
+	x.insert(i)
+x.print_stack()
