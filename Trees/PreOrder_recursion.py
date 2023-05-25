fc603e6d48abe8f12c85535cef1801cac318209a
Sriram <71959217+Sriram-bb63@users.noreply.github.com>
2023-05-25 23:20:56 +0530

    Recursive tree traversals

---
+++ Trees/PreOrder_recursion.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def __repr__(self) -> str:
+        return self.val
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+def traverse(node) -> None:
+    if node:
+	print(node.val)
+        traverse(node.left)
+        traverse(node.right)
+
+traverse(root)
